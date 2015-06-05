57f4ffb0f3119993e009fb59491437a9d81dad83
Yen <winds0915@gmail.com>
2015-06-05 10:24:10 +0800

    remove folder

--- src/tw/com/riversoft/core/struts2/rule/B2CLoginAware.java
+++
@@ -1,5 +0,0 @@
-package tw.com.riversoft.core.struts2.rule;
-
-public interface B2CLoginAware {
-
-}
--- src/tw/com/riversoft/core/struts2/rule/LoginAware.java
+++
@@ -1,6 +0,0 @@
-package tw.com.riversoft.core.struts2.rule;
-
-
-public interface LoginAware {
-
-}
